af33c2d5c7d7562360477533ed73e6cd7b552032
Jeff Browning <jeffbrowning05@gmail.com>
2014-07-23 17:31:54 -0700

    attempting staticfiles fix

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     'endless_pagination',
     'blog',
     'django_markdown',
@@ -91,7 +91,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/blog/'
+#STATIC_URL = '/static/blog/'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
 
 from .local_settings import *
